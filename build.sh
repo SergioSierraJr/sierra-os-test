3de728e6bd0e1d6e20affd6fcb4c159e9530e0e4
SergioSierraJr <132273266+SergioSierraJr@users.noreply.github.com>
2024-11-28 16:40:15 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -ouex pipefail
 # Just to fetch repos
 RELEASE="$(rpm -E %fedora)"
 
-curl -sSLo /etc/yum.repos.d/insiders-fast.repo https://packages.microsoft.com/yumrepos/microsoft-fedora$RELEASE-insiders-fast-prod/config.repo
-
+curl -sSLo /etc/yum.repos.d/edge.repo https://packages.microsoft.com/yumrepos/edge/config.repo
+curl -sSLo /etc/yum.repos.d/vscode.repo https://packages.microsoft.com/yumrepos/vscode/config.repo
 # Installing the base desktop, just the minimal stuff becuase I dont need all the stock apps gnome comes with
-rpm-ostree install gnome-shell gnome-terminal nautilus code
+rpm-ostree install gnome-shell gnome-terminal nautilus code-insiders microsoft-edge-dev
