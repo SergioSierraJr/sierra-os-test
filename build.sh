03d410761dc69546456dc4b86ae3624a0d422429
SergioSierraJr <132273266+SergioSierraJr@users.noreply.github.com>
2024-11-28 16:34:41 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ RELEASE="$(rpm -E %fedora)"
 curl -sSLo /etc/yum.repos.d/insiders-fast.repo https://packages.microsoft.com/yumrepos/microsoft-fedora$RELEASE-insiders-fast-prod/config.repo
 
 # Installing the base desktop, just the minimal stuff becuase I dont need all the stock apps gnome comes with
-rpm-ostree install gnome-shell gnome-terminal nautilus vscode-insiders
+rpm-ostree install gnome-shell gnome-terminal nautilus code
