72a5472757c50586bb08e69ea85a14df20c22b6f
SergioSierraJr <132273266+SergioSierraJr@users.noreply.github.com>
2024-11-28 14:56:35 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
+# I dont think I need this because I wont do any piping but ill keep it just incase
 set -ouex pipefail
 
+# Just to fetch repos
 RELEASE="$(rpm -E %fedora)"
 
+curl -sSLo /etc/yum.repos.d/insiders-fast.repo https://packages.microsoft.com/config/fedora/$RELEASE/insiders-fast.repo
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-rpm-ostree install screen
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+# Installing the base desktop, just the minimal stuff becuase I dont need all the stock apps gnome comes with
+rpm-ostree gnome-shell gnome-terminal nautilus vscode-insiders
