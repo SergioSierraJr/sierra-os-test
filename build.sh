7090a18723619f9fd4b69f46a92d3d8c709cbbf7
SergioSierraJr <132273266+SergioSierraJr@users.noreply.github.com>
2024-11-28 20:56:44 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -ouex pipefail
 # Just to fetch repos
 RELEASE="$(rpm -E %fedora)"
 
-# curl -sSLo /etc/yum.repos.d/edge.repo https://packages.microsoft.com/yumrepos/edge/config.repo
-# curl -sSLo /etc/yum.repos.d/vscode.repo https://packages.microsoft.com/yumrepos/vscode/config.repo
+curl -sSLo /etc/yum.repos.d/edge.repo https://packages.microsoft.com/yumrepos/edge/config.repo
+curl -sSLo /etc/yum.repos.d/vscode.repo https://packages.microsoft.com/yumrepos/vscode/config.repo
 # Installing the base desktop, just the minimal stuff becuase I dont need all the stock apps gnome comes with
-rpm-ostree install gnome-shell gnome-terminal nautilus # code-insiders microsoft-edge-dev
+rpm-ostree install gnome-shell gnome-terminal nautilus code-insiders microsoft-edge-dev
